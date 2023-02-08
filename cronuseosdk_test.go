96aaae421fb0f9bc7ef5153df63cf7e8a1c773fa
shashimalcse <shashimalsenarath.17@cse.mrt.ac.lk>
2023-02-08 23:43:09 +0530

    add multi permission check

--- cronuseosdk_test.go
+++ cronuseosdk_test.go
@@ -5,10 +5,10 @@ import (
 	"testing"
 )
 
-func TestUsernameCheck(t *testing.T) {
+func TestUsernameCheckPermission(t *testing.T) {
 
-	cronuseo := Cronuseo("http://localhost:8080/api/v1", "super", "ZHP/IlS5nDDXoWLyqQMcds6VyUoWjl+3+MLevUSku0A=")
-	allow, err := cronuseo.CheckUser("shashimal", "read", "doc")
+	cronuseo := Cronuseo("http://localhost:8080/api/v1", "super", "g+6bFqAg+y1EswfWlxGeDSRh3+WlxsrFEBH30fK8HHg=")
+	allow, err := cronuseo.CheckPermission("shashimal", "read", "doc")
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -17,4 +17,14 @@ func TestUsernameCheck(t *testing.T) {
 	} else {
 		fmt.Println("Deny")
 	}
+}
+
+func TestUsernameCheckPermissions(t *testing.T) {
+
+	cronuseo := Cronuseo("http://localhost:8080/api/v1", "super", "g+6bFqAg+y1EswfWlxGeDSRh3+WlxsrFEBH30fK8HHg=")
+	grantedScopes, err := cronuseo.CheckPermissions("shashimal", []string{"read", "create"}, "doc")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(grantedScopes)
 }
